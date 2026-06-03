0fd25842406342adae2608a6f18031ff4caeb958
yuriz <yuri.gabriel1444@gmail.com>
2026-06-03 13:46:05 -0300

    feat: criado CT14 e classe de integracao

---
+++ src/main/java/com/restaurante/gerencia/FluxoIntegracao.java
@@ -0,0 +1,13 @@
+package com.restaurante.gerencia;
+
+/**
+ * Responsável pela integração dos módulos do sistema.
+ *
+ * CT14:
+ * Fluxo geral de integração do restaurante.
+ *
+ * Autor: Yuri
+ */
+public class FluxoIntegracao {
+
+}
---
+++ src/test/java/com/restaurante/gerencia/CT14FluxoIntegracaoTest.java
@@ -0,0 +1,22 @@
+package com.restaurante.gerencia;
+
+import org.junit.jupiter.api.Test;
+
+public class CT14FluxoIntegracaoTest {
+
+    // CT14: Fluxo geral de integração do sistema
+    @Test
+    public void testCT14_FluxoGeralDeIntegracao() {
+        // 1. Login do funcionário
+
+        // 2. Cliente ocupa uma mesa
+
+        // 3. Pedido é criado
+
+        // 4. Pedido vai para preparo/cozinha
+
+        // 5. Conta é fechada
+
+        // 6. Pagamento é realizado
+    }
+}
